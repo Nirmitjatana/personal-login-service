c410511d6f12a32622a1d178df0f32505746477b
nirmit <nirmitjatana38@gmail.com>
2022-02-23 15:28:24 +0530

    chore: initialize

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const app = express()
+require('dotenv').config();
+
+app.use(express.json())
+const users = []
+
+app.get('/get', (req,res) =>{
+    res.send(users)
+})
+
+app.post('/users', async (req,res) => {
+    try{
+        const hashed = await bcrypt.hash(req.body.password, 10)
+        const user = {password: hashed}
+        console.log(user)
+        users.push(user)
+        
+        res.status(201).send()
+    }
+    catch{
+        res.status(500).send()
+    }
+})
+
+app.post('/login', async (req,res) =>{
+    try{
+        const check = process.env.PASSWORD
+        if(await bcrypt.compare(req.body.password, check)){
+            res.status(200).send("success")
+        }
+        else{
+            res.status(401).send("not allowed")
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.listen(3000)
